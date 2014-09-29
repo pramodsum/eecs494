692865c2d708dc229b0d6df51a2910c9adf83173
Sumedha <pramods@umich.edu>
2014-09-28 23:27:09 -0400

    Added slimes

---
+++ LegendOfZelda/Assets/Scripts/SlimeAnimator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeAnimator : MonoBehaviour
+{
+		public float moveSpeed;
+		public int direction; // { up, down, right, left }
+		Vector3 colliderPosition;
+	
+		// Update is called once per frame
+		void FixedUpdate ()
+		{		
+				if (direction == 0)
+						transform.Translate (Vector2.up * moveSpeed / 100);
+				else if (direction == 1)
+						transform.Translate (-1 * Vector2.up * moveSpeed / 100);
+				else if (direction == 2)
+						transform.Translate (Vector2.right * moveSpeed / 100);
+				else 
+						transform.Translate (-1 * Vector2.right * moveSpeed / 100);
+		}
+	
+	
+		void OnTriggerEnter2D (Collider2D collider)
+		{
+				//Change direction when enemy collides with something
+				direction = Random.Range (0, 3);
+		}
+}
