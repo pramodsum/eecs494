7ca774214d9a71117eb50f33316c7becc7855331
Cody Carey <careycj@umich.edu>
2014-09-30 15:01:14 -0400

    Added heart pieces to gain health, added pillars to dungeon 2

--- LegendOfZelda/Assets/Scripts/HealthScript.cs
+++ LegendOfZelda/Assets/Scripts/HealthScript.cs
@@ -7,30 +7,38 @@ public class HealthScript : MonoBehaviour {
 	public GameObject[] HeartHalves;
 	public int MaxHeartHalves = 6;
 	public int CurrentHeartHalves = 6;
-
+	public bool GodMode = false;
 	public GameObject gameOverText;
 
 	// Use this for initialization
 	void Start () {
 		CurrentHeartHalves = MaxHeartHalves;
 	}
-	
-	// Update is called once per frame
 
 	public void Hit( ){
-		if( CurrentHeartHalves > 0 ) CurrentHeartHalves--;
+		if( CurrentHeartHalves > 0 && !GodMode) CurrentHeartHalves--;
 		HeartHalves[CurrentHeartHalves].GetComponent<Image>().color = new Color(0,0,0,0);
 	}
 
 	public void Heal(int amount){
-		CurrentHeartHalves += amount;
-		if(CurrentHeartHalves > MaxHeartHalves) CurrentHeartHalves = MaxHeartHalves;
-		for( int i = 0; i < CurrentHeartHalves; i++ ){
-			HeartHalves[CurrentHeartHalves].GetComponent<Image>().color = new Color(1,0,0,1);
+		for( int i = 0; i < amount; i++ ){
+			if(CurrentHeartHalves < MaxHeartHalves){
+				CurrentHeartHalves += 1;
+				HeartHalves[CurrentHeartHalves - 1].GetComponent<Image>().color = new Color(1,0,0,1);
+			} else {
+				break;
+			}
 		}
 	}
 
+	void FixedUpdate( )
+	{
+		if(Input.GetKeyDown(KeyCode.G)){
+			GodMode = !GodMode;
+		}
+	}	
 	void Update( ){
+
 		if(CurrentHeartHalves == 0){
 			gameOverText.SetActive(true);
 			if(Input.anyKey)
---
+++ LegendOfZelda/Assets/Scripts/HeartCollision.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartCollision : MonoBehaviour {
+
+	public int HealAmount = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if(collider.tag == "Player"){
+			HealthScript hs = Camera.main.GetComponent<HealthScript>();
+
+			if(hs.CurrentHeartHalves < hs.MaxHeartHalves){
+				Camera.main.GetComponent<HealthScript>().Heal(HealAmount);
+				Destroy(gameObject);
+			}
+
+		}
+	}
+}
