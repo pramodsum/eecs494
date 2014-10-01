fb9ba8c4deb75e5228d32c04e06e416c0e1a1488
Sumedha <pramods@umich.edu>
2014-10-01 12:04:18 -0400

    Added god mode --> Still need to add automatically unlocked doors

--- LegendOfZelda/Assets/Scripts/LockedDoor.cs
+++ LegendOfZelda/Assets/Scripts/LockedDoor.cs
@@ -1,24 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LockedDoor : MonoBehaviour {
-	public GameObject OpenDoorObject;
+public class LockedDoor : MonoBehaviour
+{
+		public GameObject OpenDoorObject;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnTriggerEnter2D(Collider2D collider){
-		Debug.Log ("We collided!");
-		if( collider.tag == "Player" && Camera.main.GetComponent<Inventory>().HasKey()){
-			Camera.main.GetComponent<Inventory>().AmountOfKeys--;
-			OpenDoorObject.SetActive(true);
-			gameObject.SetActive(false);
+		// Use this for initialization
+		void Start ()
+		{
 		}
-	}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnTriggerEnter2D (Collider2D collider)
+		{
+				Debug.Log ("We collided!");
+				if (collider.tag == "Player" && (Camera.main.GetComponent<Inventory> ().HasKey () || Camera.main.GetComponent<HealthScript> ().GodMode)) {
+						if (!Camera.main.GetComponent<HealthScript> ().GodMode)
+								Camera.main.GetComponent<Inventory> ().AmountOfKeys--;
+						OpenDoorObject.SetActive (true);
+						gameObject.SetActive (false);
+				}
+		}
 }
